075a8c768c535b79abfb84a46c24289ab25b4cb0
TimVanhoutteghem <vanhoutteghem.tim@gmail.com>
2022-11-23 22:11:21 +0100

    version 1

---
+++ RandomNumberGenerator.py
@@ -0,0 +1,17 @@
+import random
+class RandomNumberGenerator():
+
+    def RandomNumber(amount, minRange, maxRange):
+# PRECONDITION: minRange < maxRange
+# PRECONDITION: amount > 0
+
+# POSTCONDITION: a RandNumber has am "amount" times been generated between the minRange and Maxrange 
+# POSTCONDITION: a lsit with all the RandNumbers has been returned to the output      
+        RandNumberList = []
+        for n in range(amount):
+            RandNumber = random.randrange(minRange, maxRange)
+            RandNumberList.append(RandNumber)
+        return RandNumberList
+
+
+print(RandomNumberGenerator.RandomNumber(4,50,100))
